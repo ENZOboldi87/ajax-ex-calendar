e4bc763eaf51acf6e3e28bea12db00ef0383fbf3
Giuseppe Rotolo <rotolo37@gmail.com>
2020-06-29 16:03:53 +0200

    aggiornamento

--- js/myscript.js
+++ js/myscript.js
@@ -1,20 +1,13 @@
-// Creare un calendario dinamico con le festività.
- // Partiamo dal gennaio 2018 dando la possibilità di cambiare mese,
-  // gestendo il caso in cui l’API non possa ritornare festività.
-   // Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018 (unici dati disponibili sull’API).
-
-// Ogni volta che cambio mese dovrò:
-// Controllare se il mese è valido (per ovviare al problema che l’API non carichi holiday non del 2018)
-// Controllare quanti giorni ha il mese scelto formando così una lista
-// Chiedere all’api quali sono le festività per il mese scelto
-// Evidenziare le festività nella lista
-
 
 $(document).ready(function() {
-// template per i giorni
 
+  // data di riferimento
+  var date = moment({
+    'year': '2018',
+    'month': '0'
+  })
 
-// template per i mesi
+// template handlebars per i mesi
 var sourceMese = $("#mesi-template").html();
 var templateMese = Handlebars.compile(sourceMese);
 var contenuto = {
@@ -25,29 +18,52 @@ $('.mese').append(htmlMesi);
 // fine template mesi
 
 
-// parte moment js
-var dateJanuary = moment("2018-01", "YYYY-MM").daysInMonth() // 31
-var mese = "Gennaio";
+
+var giorniInUnMese = date.daysInMonth();
+
 // fine parte moment js
 
 // ciclo for per stampare i giorni del mese (in base al mese)
-for (var i = 1; i < dateJanuary + 1; i++) {
+for (var i = 1; i < giorniInUnMese + 1; i++) {
   var singoliGiorni = i;
+  var mese = "Gennaio";
+  var formatoData = date.format('YYYY-MM' + '-' + AggiungiZero(i));
   var context = {
+      data: formatoData,
       giorni: singoliGiorni + ' ' + mese
       };
+      // template handlebars per i giorni
       var sourceGiorni = $("#giorni-template").html();
       var templateGiorni = Handlebars.compile(sourceGiorni);
       var htmlGiorni = templateGiorni(context);
       $('ul').append(htmlGiorni);
+      // fine template handlebars per i giorni
 };
 
+// chiamata ajax per confrontare i giorni con le festivita
 $.ajax({
   url: 'https://flynn.boolean.careers/exercises/api/holidays?year=2018&month=0',
   type: 'GET',
   success: function (data) {
     var data = data.response;
-    console.log(data);
+    // con un ciclo for stampo i dati ricevuti dall API
+    for (var i = 0; i < data.length; i++) {
+      // per ogni elemento della lista confronto i dati
+      $('.calendario ul li').each(function() {
+        var giorni = $(this).attr('data-element');
+        // console.log(giorni);
+        var nomiFestivita = data[i].name;
+        var dateFestivita = data[i].date;
+        // se il giorno equivale ai giorni festivi provenienti dall API
+        // aggiungo classe e festivita
+        if (giorni == dateFestivita) {
+          $(this).addClass('rosso');
+          $(this).append(' <span>' + nomiFestivita + '</span>');
+        }
+      });
+
+
+    }
   },
 
   error:(function() {
@@ -56,18 +72,13 @@ $.ajax({
 
 })
 
-
-
-
-// funzione che stampa il mese nel container debug
-
-
-
-
-
-
-
-  // fine funzione debug
-
+// funzione che aggiunge uno zero se un numero e minore di 10
+function AggiungiZero(number) {
+  if (number < 10) {
+    return '0' + number;
+  } else {
+    return number;
+  }
+}
 
 });
